840acb0a29d6cbb6694c24a1b0383f432dd019c0
vitorvargasdev <instalando@protonmail.com>
2024-01-26 20:47:59 -0300

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/vitorvargasdev/go-webhook-logger
+
+go 1.21.6
---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+)
+
+const TIME_FORMAT = "20060102150405"
+
+func main() {
+	port := strings.Split(os.Args[1:][0], "--port=")[1]
+
+	http.HandleFunc("/webhook-response", func(w http.ResponseWriter, r *http.Request) {
+		text := strings.Builder{}
+
+		text.WriteString("Headers:\n\n")
+		text.WriteString("```text\n")
+
+		for k, v := range r.Header {
+			text.WriteString(k + ": " + v[0] + "\n")
+		}
+
+		text.WriteString("```\n")
+
+		text.WriteString("\nBody:\n\n")
+		text.WriteString("```json\n")
+
+		body, err := io.ReadAll(r.Body)
+
+		if err != nil {
+			panic(err)
+		}
+
+		prettyJSON, err := prettyJSON(body)
+
+		text.WriteString(prettyJSON)
+
+		text.WriteString("\n```\n")
+
+		w.WriteHeader(http.StatusOK)
+
+		date := time.Now().Format(TIME_FORMAT)
+
+		os.WriteFile("./logs/"+date+".md", []byte(text.String()), 0644)
+	})
+
+	http.ListenAndServe(":"+port, nil)
+}
+
+func prettyJSON(body []byte) (string, error) {
+	var prettyJSON bytes.Buffer
+	err := json.Indent(&prettyJSON, body, "", "\t")
+
+	if err != nil {
+		log.Println("JSON parse error: ", err)
+		return "", err
+	}
+
+	return string(prettyJSON.Bytes()), nil
+}
